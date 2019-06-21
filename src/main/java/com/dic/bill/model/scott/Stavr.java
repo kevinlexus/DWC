e0f2bc4cd4bb107958b7c0680a5bd594007f54f7
LeoLexx <factorkevin@gmail.com>
2019-06-21 09:44:13 +0300

    Исправил распр. оплаты по просьбе Кис.

--- src/main/java/com/dic/bill/model/scott/Stavr.java
+++ src/main/java/com/dic/bill/model/scott/Stavr.java
@@ -6,21 +6,24 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * Ставки рефинансирования по типам лиц.счетов
+ *
  * @author Lev
  * @version 1.00
  */
 @Entity
-@Table(name = "STAV_R", schema="SCOTT")
-@Getter @Setter
+@Table(name = "STAV_R", schema = "SCOTT")
+@Getter
+@Setter
 public class Stavr {
 
-	// Id
-	@Id
-	@Column(name = "ID", unique = true, nullable = false)
-	private Integer id;
+    // Id
+    @Id
+    @Column(name = "ID", unique = true, nullable = false)
+    private Integer id;
 
     // дата начала
     @Column(name = "DAT1", updatable = false, nullable = true)
@@ -30,15 +33,15 @@ public class Stavr {
     @Column(name = "DAT2", updatable = false, nullable = true)
     private Date dt2;
 
-	// % начисления пени
+    // % начисления пени
     @Column(name = "PROC", updatable = false, nullable = true)
     private BigDecimal proc;
 
-	// кол-во дней просрочки распространяется С
+    // кол-во дней просрочки распространяется С
     @Column(name = "DAYS1", updatable = false, nullable = true)
     private Integer days1;
 
-	// кол-во дней просрочки распространяется По
+    // кол-во дней просрочки распространяется По
     @Column(name = "DAYS2", updatable = false, nullable = true)
     private Integer days2;
 
@@ -46,5 +49,29 @@ public class Stavr {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "FK_LSK_TP", referencedColumnName = "ID", updatable = false)
     private Lst tp;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o instanceof Stavr))
+            return false;
+
+        Stavr other = (Stavr) o;
+
+        if (Objects.equals(id, other.getId())) return true;
+        if (id == null) return false;
+
+        // equivalence by id
+        return id.equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        if (id != null) {
+            return id.hashCode();
+        } else {
+            return super.hashCode();
+        }
+    }
 
 }
