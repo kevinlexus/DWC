e1b9b01b58a7bde7f1a19127513e7b22fe597639
tln <factorkevin@gmail.com>
2020-06-09 07:00:02 +0300

    Сжатие таблицы C_CHARGEPAY

--- src/main/java/com/dic/bill/dao/ChargePayDAO.java
+++ src/main/java/com/dic/bill/dao/ChargePayDAO.java
@@ -2,17 +2,45 @@ package com.dic.bill.dao;
 
 import com.dic.bill.model.scott.ChargePay;
 import com.dic.bill.model.scott.ChargePayId;
+import com.dic.bill.model.scott.Kart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 
 public interface ChargePayDAO extends JpaRepository<ChargePay, ChargePayId> {
 
-    @Query("select t from ChargePay t where " +
-            "t.period = :period and t.kart.lsk = :lsk")
-    ChargePay getByLsk(@Param("lsk") String lsk, @Param("period") String period);
+
+    /**
+     * Получить все элементы по лиц.счету, начиная с заданного периода
+     *
+     * @param lsk    - лиц. счет
+     * @param period - период
+     */
+    @Query("select t from ChargePay t "
+            + "where t.kart.lsk=?1 and "
+            + " (t.mgFrom >=?2 or ?2 between t.mgFrom and t.mgTo)")
+    List<ChargePay> getByLskPeriod(String lsk, Integer period);
+
+    /**
+     * Получить все элементы по lsk
+     *
+     * @param lsk - лиц.счет
+     */
+    @Query("select t from ChargePay t "
+            + "where t.kart.lsk = ?1")
+    List<ChargePay> getByLsk(String lsk);
+
+
+    /**
+     * Получить все элементы Kart, >= заданного лс, по которым есть записи AkartPr
+     *
+     * @param firstLsk - заданный лс
+     */
+    @Query("select distinct t from ChargePay a join a.kart t "
+            + " where t.id >= ?1 order by t.id")
+    List<Kart> getAfterLsk(String firstLsk);
+
 
 }
--- src/main/java/com/dic/bill/model/scott/ChargePay.java
+++ src/main/java/com/dic/bill/model/scott/ChargePay.java
@@ -1,16 +1,11 @@
 package com.dic.bill.model.scott;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
+import com.dic.bill.Compress;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Formula;
+
+import javax.persistence.*;
 
 /**
  * Задолженности по периодам
@@ -19,35 +14,79 @@ import lombok.Setter;
  */
 @SuppressWarnings("serial")
 @Entity
-@Table(name = "C_CHARGEPAY", schema="SCOTT")
+@Table(name = "C_CHARGEPAY2", schema="SCOTT")
 @IdClass(ChargePayId.class) // суррогатный первичный ключ
 @Getter @Setter
-public class ChargePay implements java.io.Serializable {
+public class ChargePay implements java.io.Serializable, Compress {
 
 	public ChargePay() {
 	}
 
-    @Id
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_C_Chargepay2_id")
+	@SequenceGenerator(name = "SEQ_C_Chargepay2_id", sequenceName = "scott.c_chargepay2_id", allocationSize = 1)
+	@Column(name = "ID", updatable = false, nullable = false)
+	private Integer id;
+
 	// лиц.счет
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="LSK", referencedColumnName="LSK")
 	private Kart kart;
 
-    @Id
 	@Column(name = "type", updatable = false, nullable = false)
 	private Integer type; // Тип записи, 0 - начисление, 1 - оплата
 
-    @Id
     @Column(name = "mg", updatable = false, nullable = false)
 	private String mg; // период задолженности
-
-    @Id
-    @Column(name = "period", updatable = false, nullable = false)
-	private String period; // период бухгалтерский
 
     @Column(name = "summa", updatable = false, nullable = false)
 	private Double summa; // сумма задолженности
 
+	// Начало действия записи
+	@Column(name = "mgfrom", nullable = false)
+	private Integer mgFrom;
 
+	// Начало действия записи
+	@Column(name = "mgto", nullable = false)
+	private Integer mgTo; // Начало действия записи
+
+	// ключ, по которому фильтровать сравниваемые кортежи
+	@Formula("concat(TYPE,MG)")
+	private String key;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || !(o instanceof ChargePay))
+			return false;
+
+		ChargePay other = (ChargePay)o;
+
+		if (id == other.getId()) return true;
+		if (id == null) return false;
+
+		// equivalence by id
+		return id.equals(other.getId());
+	}
+
+	@Override
+	public boolean isTheSame(Compress compr) {
+		ChargePay chargePay = (ChargePay) compr;
+
+		if (kart != null ? !kart.equals(chargePay.kart) : chargePay.kart != null) return false;
+		if (type != null ? !type.equals(chargePay.type) : chargePay.type != null) return false;
+		if (mg != null ? !mg.equals(chargePay.mg) : chargePay.mg != null) return false;
+		return summa != null ? summa.equals(chargePay.summa) : chargePay.summa == null;
+
+	}
+
+	@Override
+	public int getHash() {
+		int result = kart != null ? kart.hashCode() : 0;
+		result = 31 * result + (type != null ? type.hashCode() : 0);
+		result = 31 * result + (mg != null ? mg.hashCode() : 0);
+		result = 31 * result + (summa != null ? summa.hashCode() : 0);
+		return result;
+	}
 }
 
