7947e493b613aa216b48187e594709d04d7b9009
tln <factorkevin@gmail.com>
2021-02-04 17:33:28 +0300

    Исправил начисление услуги "компонента нагрева г.в."

--- src/main/java/com/dic/bill/dao/NaborDAO.java
+++ src/main/java/com/dic/bill/dao/NaborDAO.java
@@ -1,14 +1,20 @@
 package com.dic.bill.dao;
 
+import com.dic.bill.model.scott.Kart;
 import com.dic.bill.model.scott.Nabor;
+import com.dic.bill.model.scott.Usl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 
 public interface NaborDAO extends JpaRepository<Nabor, Integer> {
 
     @Query("select t from Nabor t where t.kart.lsk = :lsk and t.usl.id = :uslId")
     Nabor getByLskUsl(@Param("lsk") String lsk, @Param("uslId") String uslId);
+
+    Optional<Nabor> findByKartAndUsl(Kart kart, Usl usl);
 
 }
--- src/main/java/com/dic/bill/dao/UslDAO.java
+++ src/main/java/com/dic/bill/dao/UslDAO.java
@@ -1,14 +1,12 @@
 package com.dic.bill.dao;
 
 import com.dic.bill.model.scott.Usl;
-import com.dic.bill.model.sec.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UslDAO extends JpaRepository<Usl, String> {
 
-	@Query("select t from Usl t where t.cd = :cd")
-	Usl getByCd(@Param("cd") String cd);
+	Optional<Usl> findByCd(String cd);
 
 }
